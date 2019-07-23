a2d89535ac7d0997e41b2b8e5f2f51997131a096
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-07-23 17:19:06 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -10,12 +10,6 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.boolean :still_in_business
       t.datetime :opening_time
       t.datetime :closing_time
-=======
-class CostumeStores < ActiveRecord::Migration[5.1]
-  def change
-    create_table :students do |t|
-      t.string :name
->>>>>>> 9c8b9ffcbf9f9689644105b444c32434e39c6220
     end 
   end 
 end
